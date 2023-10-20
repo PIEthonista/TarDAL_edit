9bead4f01442d2d557f21c1c56036742674ed673
Chun Chet Ng <ngchunchet95@gmail.com>
2023-10-20 08:45:59 +0800

    updated ssh conda env

--- ssh.sh
+++ ssh.sh
@@ -3,5 +3,5 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519_yx
 eval "$(conda shell.bash hook)"
-conda activate pix2pix_simple
+conda activate tardal_edit
 wandb login --relogin 6b305360cd440b1f7432d6f1ba4d81e0c0a60536
