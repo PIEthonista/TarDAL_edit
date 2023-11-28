bf29793d55adffda8a9d9e5037c3f5d660c8af92
Chun Chet Ng <ngchunchet95@gmail.com>
2023-11-29 00:55:06 +0800

    twcc

---
+++ slurm/run_train_tardal_dt.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#SBATCH -A MST111109
+#SBATCH -J run_train_tardal_dt
+#SBATCH -p gp4d
+#SBATCH -e twcc_logs/run_train_tardal_dt.txt
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+python train.py --cfg config/official/train/tardal-dt.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_dt_20231129_default
---
+++ slurm/run_train_tardal_tt.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#SBATCH -A MST111109
+#SBATCH -J run_train_tardal_tt
+#SBATCH -p gp4d
+#SBATCH -e twcc_logs/run_train_tardal_tt.txt
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+python train.py --cfg config/official/train/tardal-tt.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_tt_20231129_default
