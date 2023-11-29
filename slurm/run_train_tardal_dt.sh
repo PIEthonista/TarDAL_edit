a661639c664a664893f945d03250046918e2f3ca
Chun Chet Ng <ngchunchet95@gmail.com>
2023-11-29 10:40:58 +0800

    twcc

--- slurm/run_train_tardal_dt.sh
+++ slurm/run_train_tardal_dt.sh
@@ -9,5 +9,5 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 544614
+# 544717
 python train.py --cfg config/official/train/tardal-dt.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_dt_20231129_default
--- slurm/run_train_tardal_tt.sh
+++ slurm/run_train_tardal_tt.sh
@@ -9,5 +9,5 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 544613
+# 544718
 python train.py --cfg config/official/train/tardal-tt.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_tt_20231129_default
