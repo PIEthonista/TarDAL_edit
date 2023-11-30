24f7aae759247e7739f78ada92fe4e887d66b586
Chun Chet Ng <ngchunchet95@gmail.com>
2023-11-30 19:39:19 +0800

    twcc

--- slurm/run_train_tardal_ct.sh
+++ slurm/run_train_tardal_ct.sh
@@ -9,5 +9,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 544715
-python train.py --cfg config/official/train/tardal-ct.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_ct_20231129_default
+
+# # 544715
+# python train.py --cfg config/official/train/tardal-ct.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_ct_20231129_default
+
+# 545746
+python train.py --cfg config/official/train/tardal-ct.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_ct_20231130_default_without_pretrained_fusionnet
---
+++ slurm/run_train_tardal_ct_yolov5x_epoch_1000.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#SBATCH -A MST111109
+#SBATCH -J run_train_tardal_ct_yolov5x_epoch_1000
+#SBATCH -p gp4d
+#SBATCH -e twcc_logs/run_train_tardal_ct_yolov5x_epoch_1000.txt
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+# 545748
+python train.py --cfg config/official/train/tardal-ct-yolov5x-epoch-1000.yaml --auth 6b305360cd440b1f7432d6f1ba4d81e0c0a60536 --run_name tardal_ct_20231130_yolov5x_epoch_1000
